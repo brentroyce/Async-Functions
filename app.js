c05822d7d471cf8b3b2434b98d782e51e96458b5
brentroyce <78892076+brentroyce@users.noreply.github.com>
2021-11-05 20:29:18 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+// async function hello() {
+
+// }
+
+// const sing = async () => {
+//     throw "problem"
+//     return 'LALALA'
+// }
+
+
+
+// sing()
+//     .then((data) => {
+//         console.log("Promise resolved with data:", data)
+//     })
+//     .catch((err) => {
+//         console.log("promise rejected")
+//         console.log(err)
+//     }
+
+// const delayedColorChange = (color,delay) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.backgroundColor  = color;
+//             resolve();
+//         }, delay)
+//     })
+// }
+
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('yellow', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('purple', 1000))
+    
+
+// async function rainbow() {
+//     await delayedColorChange('red', 1000)
+//     await delayedColorChange('orange', 1000)
+//     await delayedColorChange('yellow', 1000)
+//     await delayedColorChange('green', 1000)
+//     await delayedColorChange('blue', 1000)
+//     await delayedColorChange('purple', 1000)
+//     return "All Done"
+// }
+
+// // rainbow().then(() => console.log("End of rainbow"))
+
+// async function printRainbow() {
+//     await rainbow();
+//     console.log("End of rainbow")
+// }
+
+// printRainbow()
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+async function makeTwoRequests(){
+    try {
+    let data1 = await fakeRequest('/page1');
+    console.log(data1)
+    let data2 = await fakeRequest('/page2')
+    console.log(data2);
+    } catch (e) {
+        console.log("ERROR")
+        console.log("error is:", e)
+    }
+}
